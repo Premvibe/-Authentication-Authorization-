fc968814dc23ab5d35be01f43e27434c9066eb7a
Prem <prem7736k@gmail.com>
2026-01-03 10:26:45 +0530

    bcrypt

--- app.js
+++ app.js
@@ -17,26 +17,23 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.post('/create', async (req, res) => {
+app.post('/create',  (req, res) => {
     let {username, email, password, age} = req.body;
 
-    bcrypt.genSalt(10, function(err, salt) {
-        bcrypt.hash(password, salt, function(err, hash) {
-            if(err) {
-                console.error(err);
-                return res.status(500).send('Error hashing password');
-            }
-            password = hash;
-        });
-    });
+    bcrypt.genSalt(10,(err, salt)  => {
+        bcrypt.hash(password, salt,async (err, hash) =>{
 
+      
    let createdUser = await userModel.create({
     username,
     email,
-    password,
+    password : hash,
     age
    });  
    res.send(createdUser); 
+        });
+    });
+
 }
 );
 
